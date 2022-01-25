7e9e0fb7bb60a50a0c5f6e8c68de011d45463e98
David <32614126+Davidk20@users.noreply.github.com>
2022-01-25 22:34:09 +0000

    links now dynamically added including correct styling and images for each link.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // Expands the dropdown categories on mobile or smaller devices where the compact view is used.
 async function expand(id){
-    var x = id + "Dropdown"
-    document.getElementById(x).classList.toggle("show");
+    document.getElementById(id).classList.toggle("show");
 }
 
 // Builds a list of links onto the homepage from the JSON file stored externally
 async function buildLinks(){
+    // Loads the JSON containing the links
     collectionJson = await fetch("/collection.json");
     collection = await collectionJson.json();
+    // variable representing the div which will contain all categories
     const container = document.getElementById("categories");
+    // Static list of all category ID's in the JSON
+    const idList = [];
+    // Iterates through each category
     for (category of collection.collection) {    
-
+        idList.push(category.category);
         // Builds header and div container
         var newCategory = document.createElement("div");
         newCategory.classList.add("category");
@@ -21,30 +25,38 @@ async function buildLinks(){
         newHeader.appendChild(headerText);
         newCategory.appendChild(newHeader);
         container.appendChild(newCategory);
-
-        // Uncomment when links content filled out
-        //newHeader.addEventListener("click", expand(category.category));
-
         // Builds dropdown of links
         var newDropdown = document.createElement("div");
         newDropdown.classList.add("links");
         newDropdown.setAttribute("id", (category.category + "Dropdown"));
-        
+        // Iterates over links to build them
         for (link of category.links) {
             var newLink = document.createElement("a");
+            newLink.classList.add("link");
             newLink.setAttribute("href", link.link);
             var newLinkDescription = document.createElement("div");
             var newLinkImage = document.createElement("img");
-            //newLinkImage.src = link.link;
+            newLinkImage.src = link.icon;
             var newLinkName = document.createElement("span");
             newLinkName.textContent=link.name;
+            newLinkDescription.appendChild(newLinkImage);
             newLinkDescription.appendChild(newLinkName);
             newLink.appendChild(newLinkDescription);
             newDropdown.appendChild(newLink);
         }
         
-        newCategory.appendChild(newDropdown);
+        newCategory.appendChild(newDropdown); 
     }
+
+    configureEventListener(idList);
 }
 
 
+// Configures the document to trigger the dropdown on the correct section
+function configureEventListener(idList) {
+    document.addEventListener('click', function(event){
+        if (event.target && idList.includes(event.target.id)) {
+            expand(event.target.id + "Dropdown");
+        }
+    });
+}
